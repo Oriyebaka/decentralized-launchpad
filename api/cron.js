e856dcbeea57ad74c57b6894e588543a35e51e06
Oriyebaka <45126220+Oriyebaka@users.noreply.github.com>
2025-01-05 07:53:26 +0100

    Update cron.js

--- api/cron.js
+++ api/cron.js
@@ -1,7 +1,11 @@
 export default function handler(req, res) {
-  if (req.headers.get('Authorization') !== `Bearer ${process.env.CRON_SECRET}`) {
+  // Access the Authorization header directly
+  const authHeader = req.headers['authorization'];
+
+  if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
     return res.status(401).end('Unauthorized');
   }
 
+  // Main logic of the cron job
   res.status(200).end('Hello Cron!');
 }
